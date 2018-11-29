329386648c5502acdb51cc77658e2f327b34977a
cybor97 <muhametshin.1997@gmail.com>
2018-11-29 20:54:12 +0300

    Made it a bit smarter :)

--- main.js
+++ main.js
@@ -8,12 +8,7 @@ function onBigRedButtonClick(e) {
     chrome.tabs.getSelected(null, function (tab) {
         if (!/^chrome:\/\//.exec(tab.url)) {
             chrome.tabs.executeScript({
-                code: `(${() => {
-                    document.querySelectorAll('video, img')
-                        .forEach(element => {
-                            element.classList.add('worksafe-hidden');
-                        });
-                }})();`
+                code: `(${() => document.body.classList.add('worksafe-hidden')})();`
             });
         }
     });
