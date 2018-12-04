132d668061a74a09958f9fdc77867ae43cb74bb3
Ruslan <muhametshin.1997@gmail.com>
2018-12-04 18:01:12 +0300

    Improved lifecycle

--- main.js
+++ main.js
@@ -4,17 +4,20 @@ function init() {
     setTimeout(() => {
         bigRedButton = document.getElementById('bigRedButton');
         updateBigRedButtonState();
-        bigRedButton.onclick = () => {
-            if (!!localStorage.getItem('hiding'))
-                localStorage.removeItem('hiding');
-            else localStorage.setItem('hiding', true);
+        bigRedButton.onclick = updateState;
 
-            updateBigRedButtonState();
-
-            chrome.tabs.query({}, tabs => tabs.forEach(doWork));
-            chrome.tabs.onCreated = doWork;
-        }
+        chrome.tabs.onCreated.addListener(updateState);
+        chrome.tabs.onActivated.addListener(updateState);
     }, 1);
+}
+
+function updateState() {
+    if (!!localStorage.getItem('hiding'))
+        localStorage.removeItem('hiding');
+    else localStorage.setItem('hiding', true);
+
+    updateBigRedButtonState();
+    chrome.tabs.query({ active: true }, tabs => tabs.forEach(doWork));
 }
 
 function updateBigRedButtonState() {
@@ -22,14 +25,19 @@ function updateBigRedButtonState() {
 }
 
 function doWork(tab) {
-    console.log(tab.url);
-    if (!/^chrome:\/\//.exec(tab.url)) {
-        let hiding = !!localStorage.getItem('hiding');
-        chrome.tabs.executeScript(tab.id, {
-            code: `(${hiding => {
-                document.body.classList.toggle('worksafe-hidden', hiding);
-            }})(${hiding});`
-        });
+    if (tab) {
+        console.log(tab.url);
+        if (!/^chrome:\/\//.exec(tab.url)) {
+            let hiding = !!localStorage.getItem('hiding');
+            chrome.tabs.executeScript(tab.id, {
+                code: `(${hiding => {
+                    document.body.classList.toggle('worksafe-hidden', hiding);
+                }})(${hiding});`
+            });
+        }
+    }
+    else {
+        console.error('Tab was null or undefined!');
     }
 }
 
