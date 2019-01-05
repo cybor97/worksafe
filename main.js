b961aa9a0eb66480f4157050b63513db6ecd381f
cybor97 <muhametshin.1997@gmail.com>
2019-01-05 18:01:57 +0300

    Implemented import/export

--- main.js
+++ main.js
@@ -25,6 +25,38 @@ function init() {
         domainsUL = document.getElementById('domainsUL');
 
         updateDomainsList();
+
+
+        let fileInput = document.createElement('input');
+        fileInput.type = 'file';
+        fileInput.accept = 'application/json';
+        fileInput.style.display = 'none';
+        document.body.appendChild(fileInput);
+
+        let importDomainsButton = document.getElementById('importDomainsButton') || document.createElement('button');
+        importDomainsButton.id = 'importDomainsButton';
+        importDomainsButton.innerText = 'Import domains list';
+        importDomainsButton.onclick = () => fileInput.click();
+        importDomainsButton.style.margin = '5px 0px 5px 0px';
+        document.body.appendChild(importDomainsButton);
+
+        fileInput.addEventListener('change', e => {
+            if (e.target.files && e.target.files.length) {
+                const reader = new FileReader();
+                reader.onload = event => {
+                    let domains = JSON.parse(event.target.result);
+                    for (let domain of domains) {
+                        localStorage.setItem(domain, true);
+                        updateDomainsList();
+                        updateState();
+                        chrome.tabs.getSelected(null, tab => updateBigRedButton(getOrigin(tab.url)));
+                        fileInput.value = '';
+                    }
+                };
+                reader.onerror = error => reject(error);
+                reader.readAsText(e.target.files[0]);
+            }
+        });
     });
 }
 
@@ -34,7 +66,8 @@ function updateBigRedButton(tabOrigin) {
 
 function updateDomainsList() {
     domainsUL.innerHTML = '';
-    getDomains().forEach(c => {
+    let domains = getDomains();
+    domains.forEach(c => {
         let domainLI = document.createElement('li');
         let domainTextInput = document.createElement('input');
         let domainRemoveButton = document.createElement('a');
@@ -62,6 +95,25 @@ function updateDomainsList() {
         domainLI.appendChild(domainRemoveButton);
         domainsUL.appendChild(domainLI);
     });
+    if (domains.length) {
+        let downloadDomainsButton = document.getElementById('downloadDomainsButton') || document.createElement('button');
+        downloadDomainsButton.id = 'downloadDomainsButton';
+        downloadDomainsButton.innerText = 'Download domains list';
+        downloadDomainsButton.onclick = () => {
+            var element = document.createElement('a');
+            element.setAttribute('href', 'data:text/text;charset=utf-8,' + encodeURI(JSON.stringify(domains)));
+            element.setAttribute('download', 'domains.json');
+            element.click();
+            element.remove();
+        };
+        document.body.appendChild(downloadDomainsButton);
+    }
+    else {
+        let downloadDomainsButton = document.getElementById('downloadDomainsButton');
+        if (downloadDomainsButton) {
+            downloadDomainsButton.remove();
+        }
+    }
 }
 
 init();
